d0cf873fdcf292d3199558ff06746f486e3f1b68
rlowell-uncc <rlowell1@uncc.edu>
2024-09-23 19:50:37 -0400

    Update main.go
    added temp code for testing

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	api "API-Project-PGL/api_retreival"
 	"API-Project-PGL/fileops"
 	"fmt"
 )
 
 func main() {
+	tempslice := []string{"Italian", "dessert"}
+
+	api.GetRandomRecipe(tempslice)
+
+	return
+
 	err := fileops.WriteToFile("testfile.txt", "Hello, World!")
 	if err != nil {
 		fmt.Println(err)
