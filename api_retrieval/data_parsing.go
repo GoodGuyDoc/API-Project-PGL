db07b5a326a2b1073c59e8978a8048e687524f57
rlowell-uncc <rlowell1@uncc.edu>
2024-09-30 19:34:20 -0400

    Add error handling
    Added error handling and quota checking for spoonacular API.

--- api_retrieval/data_parsing.go
+++ api_retrieval/data_parsing.go
@@ -1,6 +1,8 @@
 package api_retrieval
 
 import (
+	"errors"
+	"fmt"
 	"io"
 	"net/http"
 )
@@ -12,6 +14,16 @@ func getSpoonData(request string) (data []byte, err error) {
 		return nil, err
 	}
 	defer resp.Body.Close()
+
+	if resp.StatusCode == 402 {
+		return nil, errors.New("out of API requests for the day")
+	} else if resp.StatusCode == 429 {
+		return nil, errors.New("slow down! too many requests. Try again later")
+	}
+
+	pointsUsed := resp.Header.Get("X-API-Quota-Request")
+	pointsLeft := resp.Header.Get("X-API-Quota-Left")
+	fmt.Printf("This search used %s points. There are %s points left today.\n", pointsUsed, pointsLeft)
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
