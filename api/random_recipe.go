66d6da2471197758a10c777a015ffaac3b612aed
rlowell-uncc <rlowell1@uncc.edu>
2024-11-04 17:07:48 -0500

    random_recipe.go now uses send_api_call function

--- api/random_recipe.go
+++ api/random_recipe.go
@@ -1,10 +1,7 @@
 package api
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
 	"os"
 )
 
@@ -39,47 +36,9 @@ type Step struct {
 // fetches a random recipe from the Spoonacular API
 func GetRandomRecipes(count int) ([]Recipe, error) {
 	apiUrl := fmt.Sprintf("https://api.spoonacular.com/recipes/random?apiKey=%s&number=%d", API_KEY, count)
-	resp, err := http.Get(apiUrl)
+	recipeResponse, err := send_api_call(apiUrl)
 	if err != nil {
 		return nil, fmt.Errorf("error making request to Spoonacular API: %w", err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, fmt.Errorf("error reading response body: %w", err)
-	}
-
-	// Pretty-print the JSON response using json.MarshalIndent
-	var prettyJSON map[string]interface{}
-	err = json.Unmarshal(body, &prettyJSON)
-	if err != nil {
-		return nil, fmt.Errorf("error unmarshaling JSON for pretty print: %w", err)
-	}
-
-	indentedJSON, err := json.MarshalIndent(prettyJSON, "", "  ")
-	if err != nil {
-		return nil, fmt.Errorf("error formatting JSON: %w", err)
-	}
-
-	// Log the formatted JSON to a file named response_log.txt
-	err = logToFile("response_log.txt", indentedJSON)
-	if err != nil {
-		return nil, fmt.Errorf("error logging to file: %w", err)
-	}
-
-	var recipeResponse RecipeResponse
-	err = json.Unmarshal(body, &recipeResponse)
-	if err != nil {
-		return nil, fmt.Errorf("error parsing JSON: %w", err)
-	}
-
-	if len(recipeResponse.Recipes) == 0 {
-		return nil, fmt.Errorf("no recipes found")
 	}
 
 	return recipeResponse.Recipes, nil
