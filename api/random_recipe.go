462c9a1b6b67a0f69edfef2e1d6c292ec905828d
rlowell-uncc <rlowell1@uncc.edu>
2024-11-04 17:47:55 -0500

    Add GetRandomRecipeByTag func

--- api/random_recipe.go
+++ api/random_recipe.go
@@ -3,6 +3,7 @@ package api
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 const API_KEY = "a867e9b240a645c3a08192f8d6b8b61c"
@@ -33,9 +34,21 @@ type Step struct {
 	Step   string `json:"step"`
 }
 
-// fetches a random recipe from the Spoonacular API
+// Returns count amount of random recipes from spoonacular api
 func GetRandomRecipes(count int) ([]Recipe, error) {
 	apiUrl := fmt.Sprintf("https://api.spoonacular.com/recipes/random?apiKey=%s&number=%d", API_KEY, count)
+	recipeResponse, err := getRecipeResponse(apiUrl)
+	if err != nil {
+		return nil, fmt.Errorf("error making request to Spoonacular API: %w", err)
+	}
+
+	return recipeResponse.Recipes, nil
+}
+
+// Returns count amount of random recipes from spoonacular api that are tagged with the specified tags
+func GetRandomRecipesByTag(count int, tags []string) ([]Recipe, error) {
+	includeTags := strings.Join(tags, ",")
+	apiUrl := fmt.Sprintf("https://api.spoonacular.com/recipes/random?apiKey=%s&number=%d&include-tags=%s", API_KEY, count, includeTags)
 	recipeResponse, err := getRecipeResponse(apiUrl)
 	if err != nil {
 		return nil, fmt.Errorf("error making request to Spoonacular API: %w", err)
