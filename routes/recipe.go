3d2bdd558361574e95c8e7195e9b785ed5d7dac8
ThomasMcCauley <tmccaul4@uncc.edu>
2024-11-04 22:57:55 -0500

    Added Route for GetRandomREcipesByTag
    Handler function "RecipeByTagHandler" reads query parameters from the request from the front-end and calls GetRandomRecipesByTag with those parameters

--- routes/recipe.go
+++ routes/recipe.go
@@ -6,10 +6,13 @@ import (
 	"html/template"
 	"net/http"
 	"spoonacular-api/api"
+	"strconv"
+	"strings"
 )
 
 func SetupRecipeRoutes() {
 	http.HandleFunc("/api/recipes", RecipeHandler)
+	http.HandleFunc("/api/recipes/byTag", RecipeByTagHandler)
 	http.HandleFunc("/api/recipe/", RecipeDetailHandler)
 	http.HandleFunc("/recipe/", RecipeDetailPageHandler)
 }
@@ -21,6 +24,35 @@ func RecipeHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		w.Header().Set("Content-Type", "application/json")
 		http.Error(w, `{"error": "Failed to get random recipes"}`, http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(recipes)
+}
+
+func RecipeByTagHandler(w http.ResponseWriter, r *http.Request) {
+	// parse count
+	countParam := r.URL.Query().Get("count")
+	count := 5 // Default to 5 if no count given
+	if countParam != "" {
+		parsedCount, err := strconv.Atoi(countParam)
+		if err == nil {
+			count = parsedCount
+		}
+	}
+
+	// parse tags
+	tagsParam := r.URL.Query().Get("tags")
+	tags := []string{}
+	if tagsParam != "" {
+		tags = strings.Split(tagsParam, ",")
+	}
+
+	recipes, err := api.GetRandomRecipesByTag(count, tags)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, `{"error": "Failed to get recipes by tag"}`, http.StatusInternalServerError)
 		return
 	}
 
