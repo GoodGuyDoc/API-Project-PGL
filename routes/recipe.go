34d3f94f1ea1874c2d5b20ea4747dd2e0b6faccc
rlowell-uncc <rlowell1@uncc.edu>
2024-11-17 17:42:35 -0500

    add api route & modify api route
    add similarRecipe and modify random recipe bytag route

--- routes/recipe.go
+++ routes/recipe.go
@@ -7,7 +7,6 @@ import (
 	"net/http"
 	"spoonacular-api/api"
 	"strconv"
-	"strings"
 )
 
 func SetupRecipeRoutes() {
@@ -16,12 +15,13 @@ func SetupRecipeRoutes() {
 	http.HandleFunc("/api/recipe/", RecipeDetailHandler)
 	http.HandleFunc("/recipe_detail", RecipeDetailPageHandler)
 	http.HandleFunc("/api/convert", ConversionHandler)
+	http.HandleFunc("/api/similarRecipe/", SimilarRecipeHandler)
 	http.HandleFunc("/random_recipe_page", RandomRecipePageHandler)
 }
 
 // RecipeHandler fetches random recipes and serves them as JSON back to the frontend.
 func RecipeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Fetching random recipes...")
+	// fmt.Println("Fetching random recipes...")
 	recipes, err := api.GetRandomRecipes(5) //get 5 random recipes
 	if err != nil {
 		fmt.Println(err)
@@ -34,10 +34,11 @@ func RecipeHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(recipes)
 }
 
+// Gets recipes by tag, with an optional count parameter. Usage: /api/recipes/byTag?includeTags=tag1,tag2&excludeTags=tag3,tag4&count=4
 func RecipeByTagHandler(w http.ResponseWriter, r *http.Request) {
 	// parse count
 	countParam := r.URL.Query().Get("count")
-	count := 5 // Default to 5 if no count given
+	count := 1 // Default to 1 if no count given
 	if countParam != "" {
 		parsedCount, err := strconv.Atoi(countParam)
 		if err == nil {
@@ -45,14 +46,15 @@ func RecipeByTagHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	// parse tags
-	tagsParam := r.URL.Query().Get("tags")
-	tags := []string{}
-	if tagsParam != "" {
-		tags = strings.Split(tagsParam, ",")
-	}
+	// parse include tags
+	includeTags := r.URL.Query().Get("includeTags")
+	// tags := []string{}
+	// if includeTagsParam != "" {
+	// 	tags = strings.Split(includeTagsParam, ",")
+	// }
+	excludeTags := r.URL.Query().Get("excludeTags")
 
-	recipes, err := api.GetRandomRecipesByTag(count, tags)
+	recipes, err := api.GetRandomRecipesByTag(count, includeTags, excludeTags)
 	if err != nil {
 		w.Header().Set("Content-Type", "application/json")
 		http.Error(w, `{"error": "Failed to get recipes by tag"}`, http.StatusInternalServerError)
@@ -91,6 +93,7 @@ func RecipeDetailHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(recipe)
 }
 
+// Returns a conversion of a specific ingredient from one unit to another requested unit. Usage: /api/convert?ingredientName=flour&amount=2.5&unit=cups&convertToUnit=grams
 func ConversionHandler(w http.ResponseWriter, r *http.Request) {
 	ingredientName := r.URL.Query().Get("ingredientName")
 	amountParam := r.URL.Query().Get("amount")
@@ -118,6 +121,32 @@ func ConversionHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(conversionInfo)
 }
+
+// Returns a similar recipe. Usage: /api/similarRecipe/123982?count=3
+func SimilarRecipeHandler(w http.ResponseWriter, r *http.Request) {
+	recipeID := r.URL.Path[len("/api/similarRecipe/"):] //extract the recipe ID from the URL
+	// grab count (if provided)
+	countParam := r.URL.Query().Get("count")
+	count := 1 // Default to 1 if no count given
+	if countParam != "" {
+		parsedCount, err := strconv.Atoi(countParam)
+		if err == nil {
+			count = parsedCount
+		}
+	}
+
+	recipe, err := api.GetSimilarRecipe(recipeID, count) //get recipe details by ID
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, `{"error": "Failed to get recipe details"}`, http.StatusInternalServerError)
+		return
+	}
+
+	//send json response back to the frontend
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(recipe)
+}
+
 func RandomRecipePageHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl, err := template.ParseFiles("templates/random_recipe_page.html", "templates/header.html", "templates/footer.html")
 	if err != nil {
