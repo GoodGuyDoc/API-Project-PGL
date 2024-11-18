c5ab59035104ee469a4d06889c3687e9aa64b452
rlowell-uncc <rlowell1@uncc.edu>
2024-11-18 17:18:23 -0500

    Implement sendApiCall

--- api/similar_recipe.go
+++ api/similar_recipe.go
@@ -1,11 +1,8 @@
 package api
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
-	"net/http"
 	"testing"
 )
 
@@ -23,34 +20,14 @@ func GetSimilarRecipe(recipeId string, count int) ([]SimilarRecipe, error) {
 
 	for i := 0; i < 3; i++ {
 		apiUrl := fmt.Sprintf("https://api.spoonacular.com/recipes/%s/similar?apiKey=%s&number=%d", recipeId, API_KEY[i], count)
-		resp, err := http.Get(apiUrl)
+		err := sendApiCall(apiUrl, &recipes)
 
 		if err != nil && err.Error() == "this api key is ratelimited" { // bad api key, go next
 			continue
-		} else {
-			if err != nil {
-				return nil, fmt.Errorf("error making request to Spoonacular API: %w", err)
-			}
-			defer resp.Body.Close()
-
-			if resp.StatusCode != http.StatusOK {
-				if resp.StatusCode == 402 || resp.StatusCode == 429 {
-					return nil, fmt.Errorf("this api key is ratelimited")
-				}
-				return nil, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
-			}
-
-			body, err := io.ReadAll(resp.Body)
-			if err != nil {
-				return nil, fmt.Errorf("error reading response body: %w", err)
-			}
-
-			err = json.Unmarshal(body, &recipes)
-			if err != nil {
-				return nil, fmt.Errorf("error parsing JSON: %w", err)
-			}
-			return recipes, nil
+		} else if err != nil {
+			return nil, fmt.Errorf("error making request to Spoonacular API: %w", err)
 		}
+		return recipes, nil
 	}
 	// if we did not find a good api key, throw an error (we finished looping)
 	return nil, fmt.Errorf("error making request to Spoonacular API: %w", errors.New("all api keys are ratelimited"))
